e2ce7387d47ba4448cc54889e1980dde2b06cd44
AndreiGrozav <andrei.grozav@analog.com>
2020-10-23 16:46:40 +0300

    adv7513_de10nano: Use VHDL for synthesis

--- projects/adv7513/de10nano/system_project.tcl
+++ projects/adv7513/de10nano/system_project.tcl
@@ -4,6 +4,9 @@ source ../../scripts/adi_project_intel.tcl
 
 adi_project adv7513_de10nano
 
+exec -ignorestderr $quartus(quartus_rootpath)/sopc_builder/bin/qsys-generate \
+      system_bd.qsys --synthesis=VHDL --family=$family --part=$device \
+
 source $ad_hdl_dir/projects/common/de10nano/de10nano_system_assign.tcl
 
 execute_flow -compile
